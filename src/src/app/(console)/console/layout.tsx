9fe39bfbf22dae76885fcd5b152f296d67af652a
Hermes Worker <donotreply@agilexplus.com>
2026-09-24 03:43:40 +0000

    chore: remove accidentally committed src/src/ directory

--- src/src/app/(console)/console/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ConsoleShell from './ConsoleShell'
-
-export default function ConsoleRootLayout({ children }: { children: React.ReactNode }) {
-  return <ConsoleShell>{children}</ConsoleShell>
-}
